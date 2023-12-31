6ee1353f9a63e373cc79adb25ad392fb20b6619b
BrokenKeyboard <satasiyahet@gmail.com>
2023-12-31 13:26:15 -0800

    refactored control logic Added More info from search
    Control logic now supports movetime, infinite, node search, and depth search
    Added tt hit, and cutoff information to search info
    
    No elo impact

--- search/src/lib.rs
+++ search/src/lib.rs
@@ -66,7 +66,7 @@ impl GoInfo {
 }
 
 pub struct Search {
-    nodes: u64,
+    search_info: SearchInfo,
     shared: Arc<Mutex<Shared>>,
     table: Table,
     board: Board,
@@ -75,20 +75,61 @@ pub struct Search {
     eval: Eval,
 }
 
+struct SearchInfo {
+    nodes: u64,
+    tt_hits: u64,
+    cutoffs: u64,
+}
+
+impl SearchInfo {
+    fn new() -> SearchInfo {
+        SearchInfo {
+            nodes: 0,
+            tt_hits: 0,
+            cutoffs: 0,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.nodes = 0;
+        self.tt_hits = 0;
+        self.cutoffs = 0;
+    }
+}
+#[derive(Clone, Copy)]
 pub struct SearchData {
     killer_move: Option<Move>,
     pv_move: Option<Move>,
 }
-pub struct Controller {
-    pub end_time: Instant,
-    pub max_depth: u8,
+
+pub enum EndCondition {
+    Time(Instant),
+    Nodes(u64),
+    Depth(u8),
+    Infinite,
 }
 
+impl EndCondition {
+    pub fn met(&self, nodes: u64, depth: u8) -> bool {
+        match self {
+            EndCondition::Time(end_time) => Instant::now() >= *end_time, // did we hit the time condition?
+            EndCondition::Nodes(node_count) => nodes >= *node_count, // did we hit the node condition?
+            EndCondition::Depth(depth_to_reach) => depth == *depth_to_reach, // did we hit the depth condition?
+            EndCondition::Infinite => false, // you can never meet the end condition for infinity >:)
+        }
+    }
+}
 impl Search {
     pub fn new(shared: Arc<Mutex<Shared>>) -> Self {
         Search {
-            stack_storage: vec![],
-            nodes: 0,
+            stack_storage: vec![
+                SearchData {
+                    killer_move: None,
+                    pv_move: None
+                };
+                200
+            ],
+            search_info: SearchInfo::new(),
             table: Table::new(2_000_000),
             shared: shared,
             board: Board::new("x5o/7/7/7/7/7/o5x x 0 1".to_string()),
@@ -96,10 +137,15 @@ impl Search {
             eval: Eval::new(),
         }
     }
-    /// do the things like clearing the hash, resetting history, etc
+    /// Clear hash and reset PV on new game
     pub fn setup_newgame(&mut self) {
         self.table.reset();
-        self.stack_storage = vec![];
+        self.stack_storage.iter_mut().for_each(|x| {
+            *x = SearchData {
+                killer_move: None,
+                pv_move: None,
+            }
+        });
     }
     /// initialize the board state using stuff
     pub fn set_position(&mut self, input: String) {
@@ -129,69 +175,72 @@ impl Search {
 
     /// find the best move for a position
     pub fn find_best_move(&mut self, info: &GoInfo) {
-        let mut controller;
+        // find run mode amongst : {infinite, time, depth, nodes, movetime}
+        let end_cond;
         if info.infinite {
-            controller = Controller {
-                end_time: Instant::now() + Duration::from_secs(1000),
-                max_depth: 1,
-            }
-        } else {
+            end_cond = EndCondition::Infinite;
+        } else if let Some(nodes) = info.nodes {
+            end_cond = EndCondition::Nodes(nodes as u64);
+        } else if let Some(depth) = info.depth {
+            end_cond = EndCondition::Depth(depth as u8);
+        } else if let Some(movetime) = info.movetime {
+            end_cond = EndCondition::Time(Instant::now() + Duration::from_millis(movetime.into()));
+        } else if let (Some(btime), Some(wtime)) = (info.btime, info.wtime) {
+            let (binc, winc) = if let (Some(binc), Some(winc)) = (info.binc, info.winc) {
+                (binc, winc)
+            } else {
+                (0, 0)
+            };
             let my_time;
             let other_time;
             match self.my_side {
                 Side::Black => {
-                    my_time = info.btime.unwrap();
-                    other_time = info.wtime.unwrap();
+                    my_time = btime;
+                    other_time = wtime;
                 }
                 Side::White => {
-                    my_time = info.wtime.unwrap();
-                    other_time = info.btime.unwrap();
+                    my_time = wtime;
+                    other_time = btime;
                 }
             };
             let time_left = if other_time < my_time {
-                (my_time - other_time).max(my_time - other_time + 30)
+                (((my_time - other_time) * 3) / 4) + my_time / 10
             } else {
                 my_time / 10
             };
-            controller = Controller {
-                end_time: Instant::now() + Duration::from_millis(time_left.into()),
-                max_depth: 1,
-            };
+            end_cond = EndCondition::Time(Instant::now() + Duration::from_millis(time_left.into()));
+        } else {
+            panic!("No end condition findable!");
         }
+        // store the start time of the search for nps calcs
         let t0 = Instant::now();
+        // store the best move
         let mut bestmove = Move {
             null: false,
             from: 0,
             to: 0,
             capture_square: 0,
         };
-        self.stack_storage.push(SearchData {
-            killer_move: None,
-            pv_move: None,
-        });
-        let max_depth = if info.infinite { 255 } else { 50 };
+
+        let max_depth = 200; // randomly chosen
+        self.search_info.reset();
+
         for depth in 1..max_depth {
-            self.stack_storage.insert(
-                0,
-                SearchData {
-                    killer_move: None,
-                    pv_move: None,
-                },
-            );
-            controller.max_depth = depth;
-            self.nodes = 0;
-            let score = self.negamax(&controller, -100_000, 100_000, depth);
+            let score = self.negamax(&end_cond, -100_000, 100_000, depth);
             let t1 = Instant::now();
             println!(
-                "info depth {depth} score {score}, nps {}, nodes {}, time {}",
-                (self.nodes as f64 / (t1 - t0).as_secs_f64()) as u64,
-                self.nodes,
-                (t1 - t0).as_millis()
+                "info depth {depth} score {score}, nodes {}, time {}, tthits {}, cutoffs {}, nps {}",
+                self.search_info.nodes,
+                (t1 - t0).as_millis(), 
+                self.search_info.tt_hits,
+                self.search_info.cutoffs,
+                (self.search_info.nodes as f64 / (t1 - t0).as_secs_f64()) as u64,
             );
-            if t1 <= controller.end_time || self.shared.lock().unwrap().stop {
-                bestmove = self.stack_storage[depth as usize].pv_move.unwrap();
-            } else {
+            if end_cond.met(self.search_info.nodes, depth) || self.shared.lock().unwrap().stop {
+                self.shared.lock().unwrap().stop = false;
                 break;
+            } else {
+                bestmove = self.stack_storage[depth as usize].pv_move.unwrap();
             }
         }
 
@@ -201,12 +250,13 @@ impl Search {
     /// negamax
     pub fn negamax(
         &mut self,
-        controller: &Controller,
+        end_condition: &EndCondition,
         mut alpha: i32,
         beta: i32,
         depth: u8,
     ) -> i32 {
-        if Instant::now() > controller.end_time || self.shared.lock().unwrap().stop {
+        // only worry about nodes cause search depth isnt useful here
+        if end_condition.met(self.search_info.nodes, 0) || self.shared.lock().unwrap().stop {
             return 0;
         }
 
@@ -235,19 +285,21 @@ impl Search {
             if entry.hash == self.board.hash() {
                 // tt move
                 if moves.contains(&entry.hash_move) {
+                    self.search_info.tt_hits += 1;
                     tt_move = Some(entry.hash_move);
                 }
             }
         }
+
+        // killer move
         let mut killer_move = None;
-        // // killer move
         if let Some(killer_entry) = self.stack_storage[depth as usize].killer_move {
             let num = if let Some(tt_move) = tt_move {
                 tt_move == killer_entry
             } else {
                 false
             };
-            if moves.contains(&killer_entry) && !num {
+            if !num && moves.contains(&killer_entry) {
                 killer_move = Some(killer_entry);
             }
         }
@@ -257,9 +309,9 @@ impl Search {
         let new_moves = movepicker.sort();
 
         for mov in &new_moves {
-            self.nodes += 1;
+            self.search_info.nodes += 1;
             let delta = make_move(&mut self.board, mov);
-            let score = -self.negamax(controller, -beta, -alpha, depth - 1);
+            let score = -self.negamax(end_condition, -beta, -alpha, depth - 1);
             unmake_move(&mut self.board, mov, delta);
 
             if score > best_score {
@@ -273,7 +325,11 @@ impl Search {
             }
 
             if alpha >= beta {
-                self.stack_storage[depth as usize].killer_move = Some(*mov);
+                // update killer if quiet move and single
+                // if mov.capture_square == 0 {
+                    self.stack_storage[depth as usize].killer_move = Some(*mov);
+                // }
+                self.search_info.cutoffs += 1;
                 break;
             }
         }
